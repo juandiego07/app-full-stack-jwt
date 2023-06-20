669cd86f3675350b5ddbc051bc0a668a24d7465c
juandiego07 <juan_diego07@hotmail.com>
2023-06-19 21:56:08 -0500

    Use Link from react-route-dom in component Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Fragment>
+      <div className="navbar">
+        <Link to={"/"}>Login</Link>
+        <Link to={"/register"}>Register</Link>
+        <Link to={"/home"}>Home</Link>
+        <Link to={"/dashboard"}>Dashboard</Link>
+      </div>
+    </Fragment>
+  );
+};
