8b777dce20eeba09d0de488e7d7361a461a63a4b
juandiego07 <juan_diego07@hotmail.com>
2023-06-19 22:00:23 -0500

    Use Browser from react-route-dom in component App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./style.css";
+import { Fragment } from "react";
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
+import { Home } from "./components/Home";
+import { Dashbord } from "./components/Dashbord";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Fragment>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <div className="main">
+        <Routes>
+          <Route path="/" element={<Login></Login>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/dashboard" element={<Dashbord></Dashbord>}></Route>
+        </Routes>          
+        </div>
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
