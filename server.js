79ecdc4d0dd90ae4375c36f9ea517216b2217330
juandiego07 <juan_diego07@hotmail.com>
2023-06-21 22:26:40 -0500

    Setting server with express

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const routes = require("./routes/route.js");
 
-app.use(cors(), express.json());
+
+app.use(cors(), express.json(), routes);
 
 app.listen(process.env.PORT, () => {
   console.log(
